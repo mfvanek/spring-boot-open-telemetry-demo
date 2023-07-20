c12191ade38ac78982c2c4c4ffe97dd248825a29
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2023-07-20 11:50:28 +0400

    Added gradle-versions-plugin (#35)
    * Added gradle-versions-plugin
    
    * Updated opentelemetry
    
    ---------
    
    Co-authored-by: i.n.vakhrushev <i.n.vakhrushev@tinkoff.ru>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     id("java")
     id("org.springframework.boot") version "3.1.1"
@@ -5,6 +7,7 @@ plugins {
     id("com.bmuschko.docker-java-application") version "9.3.1"
     id("io.freefair.lombok") version "8.1.0"
     id("com.google.osdetector") version "1.7.3"
+    id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 group = "io.github.mfvanek"
@@ -24,14 +27,11 @@ dependencies {
     implementation("com.google.code.findbugs:jsr305:3.0.2")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation(enforcedPlatform("io.micrometer:micrometer-bom:1.11.2"))
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 
-    implementation(enforcedPlatform("io.micrometer:micrometer-tracing-bom:1.1.3"))
     implementation("io.micrometer:micrometer-tracing-bridge-otel")
-    implementation(enforcedPlatform("io.opentelemetry:opentelemetry-bom:1.26.0"))
-    implementation("io.opentelemetry:opentelemetry-exporter-jaeger")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
 
     // https://github.com/netty/netty/issues/11020
     if (osdetector.arch == "aarch_64") {
@@ -39,11 +39,19 @@ dependencies {
     }
 
     testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation(enforcedPlatform("org.junit:junit-bom:5.9.3"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webflux")
-    testImplementation(enforcedPlatform("org.testcontainers:testcontainers-bom:1.18.3"))
+    testImplementation(platform("org.testcontainers:testcontainers-bom:1.18.3"))
     testImplementation("org.testcontainers:junit-jupiter")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("io.micrometer:micrometer-bom:1.11.2")
+        mavenBom("io.micrometer:micrometer-tracing-bom:1.1.3")
+        mavenBom("io.opentelemetry:opentelemetry-bom:1.28.0")
+        mavenBom("org.junit:junit-bom:5.9.3")
+    }
 }
 
 tasks {
@@ -62,4 +70,20 @@ docker {
 
 springBoot {
     buildInfo()
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
+    checkForGradleUpdate = true
+    gradleReleaseChannel = "current"
+    checkConstraints = true
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
