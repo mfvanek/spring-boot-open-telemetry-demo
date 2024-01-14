213dd1b0c12cc93527e3b9af7b7fedd6e19c323a
Ivan Vakhrushev <mfvanek@gmail.com>
2024-01-14 21:34:45 +0400

    Add common BOM for internal usage (#92)

---
+++ common-internal-bom/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java-platform")
+}
+
+description = "BOM for internal usage"
+
+javaPlatform {
+    allowDependencies()
+}
+
+dependencies {
+    api(platform("org.assertj:assertj-bom:3.25.1"))
+    api(platform("org.testcontainers:testcontainers-bom:1.19.3"))
+    api(platform("org.junit:junit-bom:5.10.1"))
+}
