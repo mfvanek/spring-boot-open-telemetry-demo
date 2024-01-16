1e5b1fde0989dd10739b9945e101a686d9f71771
Ivan Vakhrushev <mfvanek@gmail.com>
2024-01-16 11:00:44 +0400

    Add traceId to the response

---
+++ spring-boot-2-demo-app/src/main/java/io/github/mfvanek/spring/test/filters/TraceIdInResponseServletFilter.java
@@ -0,0 +1,34 @@
+package io.github.mfvanek.spring.test.filters;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class TraceIdInResponseServletFilter implements Filter {
+
+    public static final String TRACE_ID_HEADER_NAME = "X-TraceId";
+
+    private final Tracer tracer;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        Span currentSpan = tracer.currentSpan();
+        if (currentSpan != null) {
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.addHeader(TRACE_ID_HEADER_NAME, currentSpan.context().traceId());
+        }
+        chain.doFilter(request, response);
+    }
+}
--- spring-boot-2-demo-app/src/test/java/io/github/mfvanek/spring/test/controllers/TimeControllerTest.java
+++ spring-boot-2-demo-app/src/test/java/io/github/mfvanek/spring/test/controllers/TimeControllerTest.java
@@ -1,12 +1,9 @@
 package io.github.mfvanek.spring.test.controllers;
 
-import io.github.mfvanek.spring.test.support.KafkaInitializer;
+import io.github.mfvanek.spring.test.support.KafkaConsumerUtils;
 import io.github.mfvanek.spring.test.support.TestBase;
 import lombok.SneakyThrows;
-import org.apache.kafka.clients.CommonClientConfigs;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.header.Header;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,23 +14,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
-import org.springframework.kafka.listener.MessageListener;
-import org.springframework.kafka.test.utils.ContainerTestUtils;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
 
 import javax.annotation.Nonnull;
+import java.nio.charset.StandardCharsets;
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+import static io.github.mfvanek.spring.test.filters.TraceIdInResponseServletFilter.TRACE_ID_HEADER_NAME;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(OutputCaptureExtension.class)
@@ -45,20 +38,12 @@ class TimeControllerTest extends TestBase {
 
     @Autowired
     private KafkaProperties kafkaProperties;
+    @Autowired
+    private Clock clock;
 
     @BeforeAll
     void setUpKafkaConsumer() {
-        final var containerProperties = new ContainerProperties(kafkaProperties.getTemplate().getDefaultTopic());
-        final Map<String, Object> consumerProperties = KafkaTestUtils.consumerProps(KafkaInitializer.getBootstrapSevers(), "sender", "false");
-        consumerProperties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
-        consumerProperties.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
-        consumerProperties.put(SaslConfigs.SASL_JAAS_CONFIG, KafkaInitializer.plainJaas());
-        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.UUIDDeserializer.class);
-        final var consumer = new DefaultKafkaConsumerFactory<UUID, String>(consumerProperties);
-        container = new KafkaMessageListenerContainer<>(consumer, containerProperties);
-        container.setupMessageListener((MessageListener<UUID, String>) consumerRecords::add);
-        container.start();
-        ContainerTestUtils.waitForAssignment(container, 1);
+        container = KafkaConsumerUtils.setUpKafkaConsumer(kafkaProperties, consumerRecords);
     }
 
     @AfterAll
@@ -77,23 +62,33 @@ class TimeControllerTest extends TestBase {
                         .build())
                 .exchange()
                 .expectStatus().isOk()
+                .expectHeader().exists(TRACE_ID_HEADER_NAME)
                 .expectBody(LocalDateTime.class)
-                .returnResult()
-                .getResponseBody();
-        assertThat(result)
-                .isBefore(LocalDateTime.now());
+                .returnResult();
+        final String traceId = result.getRequestHeaders().getFirst(TRACE_ID_HEADER_NAME);
+        assertThat(traceId).isNotBlank();
+        assertThat(result.getResponseBody())
+                .isBefore(LocalDateTime.now(clock));
         assertThat(output.getAll())
-                .contains("Called method getNow. TraceId = ")
+                .contains("Called method getNow. TraceId = " + traceId)
                 .contains("Awaiting acknowledgement from Kafka");
 
         final var received = consumerRecords.poll(10, TimeUnit.SECONDS);
         assertThat(received).isNotNull();
         assertThat(received.value()).startsWith("Current time = ");
-        final List<String> headerNames = Arrays.stream(received.headers().toArray())
+        final Header[] headers = received.headers().toArray();
+        final var headerNames = Arrays.stream(headers)
                 .map(Header::key)
                 .toList();
         assertThat(headerNames)
                 .hasSize(2)
                 .containsExactlyInAnyOrder("traceparent", "b3");
+        final var headerValues = Arrays.stream(headers)
+                .map(Header::value)
+                .map(v -> new String(v, StandardCharsets.UTF_8))
+                .toList();
+        assertThat(headerValues)
+                .hasSameSizeAs(headerNames)
+                .satisfiesExactlyInAnyOrder(h -> assertThat(h).contains(traceId));
     }
 }
