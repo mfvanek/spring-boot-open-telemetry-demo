3805de2e52c35d68535bdd4da4c384edc984bd58
Ivan Vakhrushev <mfvanek@gmail.com>
2024-08-03 21:36:32 +0400

    Configure OtlpGrpcSpanExporter bean

--- spring-boot-3-demo-app/src/test/java/io/github/mfvanek/spring/boot3/test/ApplicationTests.java
+++ spring-boot-3-demo-app/src/test/java/io/github/mfvanek/spring/boot3/test/ApplicationTests.java
@@ -1,13 +1,17 @@
 package io.github.mfvanek.spring.boot3.test;
 
+import io.github.mfvanek.spring.boot3.test.support.JaegerInitializer;
 import io.github.mfvanek.spring.boot3.test.support.TestBase;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import io.micrometer.tracing.otel.bridge.OtelTracer;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+
+import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,5 +32,13 @@ class ApplicationTests extends TestBase {
                 .isInstanceOf(OtelTracer.class)
                 .satisfies(t -> assertThat(t.currentSpan())
                         .isNotEqualTo(Span.NOOP));
+        assertThat(applicationContext.getBean("otelJaegerGrpcSpanExporter"))
+                .isNotNull()
+                .isInstanceOf(OtlpGrpcSpanExporter.class)
+                .hasToString(String.format(Locale.ROOT, "OtlpGrpcSpanExporter{exporterName=otlp, type=span, " +
+                        "endpoint=http://localhost:%d, " +
+                        "endpointPath=/opentelemetry.proto.collector.trace.v1.TraceService/Export, timeoutNanos=5000000000, " +
+                        "connectTimeoutNanos=10000000000, compressorEncoding=null, " +
+                        "headers=Headers{User-Agent=OBFUSCATED}}", JaegerInitializer.getFirstMappedPort()));
     }
 }
