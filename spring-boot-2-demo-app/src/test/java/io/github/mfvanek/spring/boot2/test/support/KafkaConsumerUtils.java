6396d9d2b0b50443a47605568ce83aec351e13ef
Ivan Vakhrushev <mfvanek@gmail.com>
2024-08-04 20:26:33 +0400

    Add Spring Boot 2 example

--- spring-boot-2-demo-app/src/test/java/io/github/mfvanek/spring/boot2/test/support/KafkaConsumerUtils.java
+++ spring-boot-2-demo-app/src/test/java/io/github/mfvanek/spring/boot2/test/support/KafkaConsumerUtils.java
@@ -25,7 +25,7 @@ public class KafkaConsumerUtils {
             @Nonnull final KafkaProperties kafkaProperties,
             @Nonnull final BlockingQueue<ConsumerRecord<UUID, String>> consumerRecords) {
         final var containerProperties = new ContainerProperties(kafkaProperties.getTemplate().getDefaultTopic());
-        final Map<String, Object> consumerProperties = KafkaTestUtils.consumerProps(KafkaInitializer.getBootstrapSevers(), "sender", "false");
+        final Map<String, Object> consumerProperties = KafkaTestUtils.consumerProps(KafkaInitializer.getBootstrapSevers(), "test-group", "false");
         consumerProperties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
         consumerProperties.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
         consumerProperties.put(SaslConfigs.SASL_JAAS_CONFIG, KafkaInitializer.plainJaas());
