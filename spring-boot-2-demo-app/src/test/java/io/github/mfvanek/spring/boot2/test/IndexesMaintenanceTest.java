92b44bcf7f6022b4747bb9ea1d9e26301a6d2b07
Ivan Vakhrushev <mfvanek@gmail.com>
2024-11-04 10:32:16 +0400

    Refactor tests

--- spring-boot-2-demo-app/src/test/java/io/github/mfvanek/spring/boot2/test/IndexesMaintenanceTest.java
+++ spring-boot-2-demo-app/src/test/java/io/github/mfvanek/spring/boot2/test/IndexesMaintenanceTest.java
@@ -3,9 +3,10 @@ package io.github.mfvanek.spring.boot2.test;
 import io.github.mfvanek.pg.common.maintenance.DatabaseCheckOnHost;
 import io.github.mfvanek.pg.common.maintenance.Diagnostic;
 import io.github.mfvanek.pg.model.DbObject;
+import io.github.mfvanek.pg.model.PgContext;
 import io.github.mfvanek.pg.model.column.Column;
-import io.github.mfvanek.pg.model.table.Table;
 import io.github.mfvanek.spring.boot2.test.support.TestBase;
+import org.assertj.core.api.ListAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,35 +26,29 @@ class IndexesMaintenanceTest extends TestBase {
     void checkPostgresVersion() {
         final String pgVersion = jdbcTemplate.queryForObject("select version();", String.class);
         assertThat(pgVersion)
-                .startsWith("PostgreSQL 16.4");
+            .startsWith("PostgreSQL 17.0");
     }
 
     @Test
     void databaseStructureCheckForPublicSchema() {
         assertThat(checks)
-                .hasSameSizeAs(Diagnostic.values());
+            .hasSameSizeAs(Diagnostic.values());
 
         checks.stream()
-                .filter(DatabaseCheckOnHost::isStatic)
-                .forEach(check -> {
-                    switch (check.getDiagnostic()) {
-                        case TABLES_WITHOUT_PRIMARY_KEY, TABLES_WITHOUT_DESCRIPTION -> assertThat(check.check())
-                                .asInstanceOf(list(Table.class))
-                                .hasSize(1)
-                                .containsExactly(Table.of("databasechangelog", 0L));
+            .filter(DatabaseCheckOnHost::isStatic)
+            .forEach(check -> {
+                final List<? extends DbObject> objects = check.check(PgContext.ofPublic(), o -> !o.getName().equalsIgnoreCase("databasechangelog"));
+                final ListAssert<? extends DbObject> checkAssert = assertThat(objects)
+                    .as(check.getDiagnostic().name());
 
-                        case COLUMNS_WITHOUT_DESCRIPTION -> assertThat(check.check())
-                                .asInstanceOf(list(Column.class))
-                                .hasSize(14)
-                                .allSatisfy(column -> assertThat(column.getTableName()).isEqualTo("databasechangelog"));
-
-                        case TABLES_WITH_MISSING_INDEXES -> assertThat(check.check())
-                                .hasSizeLessThanOrEqualTo(1); // TODO skip runtime checks after https://github.com/mfvanek/pg-index-health/issues/456
-
-                        default -> assertThat(check.check())
-                                .as(check.getDiagnostic().name())
-                                .isEmpty();
-                    }
-                });
+                if (check.getDiagnostic() == Diagnostic.COLUMNS_WITHOUT_DESCRIPTION) {
+                    assertThat(objects)
+                        .asInstanceOf(list(Column.class))
+                        .hasSize(14)
+                        .allSatisfy(column -> assertThat(column.getTableName()).isEqualTo("databasechangelog"));
+                } else {
+                    checkAssert.isEmpty();
+                }
+            });
     }
 }
--- spring-boot-3-demo-app/src/test/java/io/github/mfvanek/spring/boot3/test/support/PostgresInitializer.java
+++ spring-boot-3-demo-app/src/test/java/io/github/mfvanek/spring/boot3/test/support/PostgresInitializer.java
@@ -10,7 +10,7 @@ import org.testcontainers.utility.DockerImageName;
 
 public class PostgresInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-    private static final DockerImageName IMAGE = DockerImageName.parse("postgres:16.4");
+    private static final DockerImageName IMAGE = DockerImageName.parse("postgres:17.0");
     private static final Network NETWORK = Network.newNetwork();
     private static final PostgreSQLContainer<?> CONTAINER = new PostgreSQLContainer<>(IMAGE);
 
