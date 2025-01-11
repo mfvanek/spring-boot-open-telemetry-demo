ddaac8bd8220193a44e4e51bf086cde163a1d9cf
Ivan Vakhrushev <mfvanek@gmail.com>
2025-01-11 12:11:49 +0400

    Add SpotBugs (#207)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
     implementation("com.bmuschko:gradle-docker-plugin:9.4.0")
     implementation("gradle.plugin.com.google.gradle:osdetector-gradle-plugin:1.7.3")
     implementation("de.thetaphi:forbiddenapis:3.8")
+    implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:6.1.0")
 }
