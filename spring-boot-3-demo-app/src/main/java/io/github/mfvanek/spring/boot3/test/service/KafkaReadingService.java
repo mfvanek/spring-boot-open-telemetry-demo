d51a0ff1597fe501091710fc47f55b3895ece1f8
m.zharinova <m.zharinova@tbank.ru>
2025-01-16 12:12:34 +0500

    wrap method listen with try for mdc - spring boot 3

--- spring-boot-3-demo-app/src/main/java/io/github/mfvanek/spring/boot3/test/service/KafkaReadingService.java
+++ spring-boot-3-demo-app/src/main/java/io/github/mfvanek/spring/boot3/test/service/KafkaReadingService.java
@@ -37,18 +37,18 @@ public class KafkaReadingService {
 
     @KafkaListener(topics = "${spring.kafka.template.default-topic}")
     public void listen(ConsumerRecord<UUID, String> message, Acknowledgment ack) {
-        final Span currentSpan = tracer.currentSpan();
-        final String traceId = currentSpan != null ? currentSpan.context().traceId() : "";
         try (MDC.MDCCloseable ignored = MDC.putCloseable("tenant.name", tenantName)) {
+            final Span currentSpan = tracer.currentSpan();
+            final String traceId = currentSpan != null ? currentSpan.context().traceId() : "";
             log.info("Received record: {} with traceId {}", message.value(), traceId);
+            jdbcTemplate.update("insert into otel_demo.storage(message, trace_id, created_at) values(:msg, :traceId, :createdAt);",
+                Map.ofEntries(
+                    Map.entry("msg", message.value()),
+                    Map.entry("traceId", traceId),
+                    Map.entry("createdAt", LocalDateTime.now(clock))
+                )
+            );
+            ack.acknowledge();
         }
-        jdbcTemplate.update("insert into otel_demo.storage(message, trace_id, created_at) values(:msg, :traceId, :createdAt);",
-            Map.ofEntries(
-                Map.entry("msg", message.value()),
-                Map.entry("traceId", traceId),
-                Map.entry("createdAt", LocalDateTime.now(clock))
-            )
-        );
-        ack.acknowledge();
     }
 }
