087cb86f45105510beb46e91fde724a2a6a0fcd3
Ivan Vakhrushev <mfvanek@gmail.com>
2025-06-08 13:22:28 +0400

    Update dependencies

--- common-internal-bom/build.gradle.kts
+++ common-internal-bom/build.gradle.kts
@@ -18,8 +18,8 @@ javaPlatform {
 dependencies {
     api(platform("org.assertj:assertj-bom:3.27.3"))
     api(platform("org.testcontainers:testcontainers-bom:1.21.1"))
-    api(platform("org.junit:junit-bom:5.13.0"))
-    api(platform("io.github.mfvanek:pg-index-health-bom:0.14.6"))
+    api(platform("org.junit:junit-bom:5.13.1"))
+    api(platform("io.github.mfvanek:pg-index-health-bom:0.15.0"))
 
     constraints {
         api("org.liquibase:liquibase-core:4.32.0")
