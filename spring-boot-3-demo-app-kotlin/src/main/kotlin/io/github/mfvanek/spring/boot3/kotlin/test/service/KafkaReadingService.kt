b84dec93f120c045f76831cca2dc7aec978bf725
m.zharinova <m.zharinova@tbank.ru>
2025-08-29 19:59:35 +0500

    add task functions to kotlin module

--- spring-boot-3-demo-app-kotlin/src/main/kotlin/io/github/mfvanek/spring/boot3/kotlin/test/service/KafkaReadingService.kt
+++ spring-boot-3-demo-app-kotlin/src/main/kotlin/io/github/mfvanek/spring/boot3/kotlin/test/service/KafkaReadingService.kt
@@ -9,7 +9,10 @@ package io.github.mfvanek.spring.boot3.kotlin.test.service
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.github.oshai.kotlinlogging.withLoggingContext
+import io.micrometer.tracing.Link
 import io.micrometer.tracing.Tracer
+import io.micrometer.tracing.propagation.Propagator
+import io.opentelemetry.context.propagation.TextMapGetter
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
@@ -27,7 +30,8 @@ class KafkaReadingService(
     @Value("\${app.tenant.name}") private val tenantName: String,
     private val tracer: Tracer,
     private val clock: Clock,
-    private val jdbcTemplate: NamedParameterJdbcTemplate
+    private val jdbcTemplate: NamedParameterJdbcTemplate,
+    private val propagator: Propagator
 ) {
     @KafkaListener(topics = ["\${spring.kafka.template.default-topic}"])
     fun listen(message: ConsumerRecord<UUID, String>, ack: Acknowledgment) {
@@ -49,5 +53,75 @@ class KafkaReadingService(
                 "createdAt" to LocalDateTime.now(clock)
             )
         )
+    }
+
+    @KafkaListener(
+        id = "\${spring.kafka.opentelemetry.additional-consumer-groupId}",
+        topics = ["\${spring.kafka.opentelemetry.additional-topic}"],
+        batch = "true"
+    )
+    fun listenAdditional(records: List<ConsumerRecord<UUID, String>>, ack: Acknowledgment) {
+        // По умолчанию здесь не будет контекста трассировки. Вся обработка, связанная с трассировкой, должна быть выполнена вручную.
+        // Есть несколько вариантов:
+        //   1) создать новый спан и контекст трассировки для всех последующих операций;
+        //   2) брать контекст трассировки из каждой записи и использовать его на время обработки этой записи;
+        //   3) взять контекст трассировки из первой записи и использовать его для всех последующих операций.
+        // Рекомендуется использовать комбинацию вариантов 1 и 2.
+
+        // Реализация варианта №1: вручную создать спан, что приведет к созданию контекста трассировки.
+        // Этого можно и не делать, но тогда логирование ниже и все последующие операции будут без traceId.
+        val batchSpan = tracer.startScopedSpan("batch-processing")
+        // val batchSpan = tracer.spanBuilder()
+        //    .setParent(tracer.traceContextBuilder().build())
+        //   .start()
+        logger.info { "current span: ${tracer.currentSpan()}" }
+        try {
+            logger.info {
+                "Received from Kafka ${records.size} records"
+            } // Это сообщение будет в логах со своим собственным traceId, если создан спан выше
+            records.forEach { record ->
+                restoreContextAndProcessSingleRecordIfNeed(record, ack)
+            }
+            ack.acknowledge()
+        } catch (e: Throwable) {
+            batchSpan.error(e)
+            throw e
+        } finally {
+            batchSpan.end()
+        }
+    }
+
+    private fun restoreContextAndProcessSingleRecordIfNeed(record: ConsumerRecord<UUID, String>, ack: Acknowledgment) {
+        // Реализация варианта №2.
+        // Берём заголовок traceparent из записи и восстанавливаем контекст трассировки на основе него.
+        // В результате цепочка спанов продолжится. Все последующие вызовы пойдут с тем же самым traceId.
+        // Если в записи из Кафки не будет заголовка traceparent, то будет использоваться текущий контекст трассировки (при его наличии).
+        val kafkaPropagatorGetter = Propagator.Getter<ConsumerRecord<UUID, String>> { carrier, _ ->
+            carrier.headers().find { it.key() == "traceparent" }?.value()?.decodeToString()
+        }
+
+        val builder = propagator.extract(record, kafkaPropagatorGetter)
+        // val spanFromRecord = builder.name("processing-record-from-kafka").start()
+        val spanFromRecord = builder
+            .addLink(Link.NOOP)
+            .setParent(tracer.traceContextBuilder().build())
+            .start()
+        try {
+            tracer.withSpan(spanFromRecord).use {
+                processSingleRecordIfNeed(record, ack)
+            }
+        } catch (e: Throwable) {
+            spanFromRecord.error(e)
+            throw e
+        } finally {
+            spanFromRecord.end()
+        }
+    }
+
+    private fun processSingleRecordIfNeed(record: ConsumerRecord<UUID, String>, ack: Acknowledgment) {
+        withLoggingContext("tenant.name" to tenantName) {
+            processMessage(record)
+            ack.acknowledge()
+        }
     }
 }
